6695e680398d81c84babc5b1e9bc4d77b66f46a8
esti <estip2005@gmail.com>
2024-11-04 11:26:28 -0600

    made small changes

--- script.js
+++ script.js
@@ -65,4 +65,3 @@ class Numbers {
   console.log(n1.product()); //returns product of numbers
   console.log(n1.greaterThan(3)); //returns numbers greater than another number
   console.log(n1.howMany(3)); //return the count of a specific number
-  
